2830a745baa48c310e29ffb6a549543ecdd7e90d
g0me3 <cah4e3@gmail.com>
2020-01-04 13:50:15 +0300

    missing files

--- src/ines.cpp
+++ src/ines.cpp
@@ -211,6 +211,7 @@ static void SetInput(void) {
 		{0xb8b9aca3,	SI_UNSET,		SI_ZAPPER,		SIFC_NONE		},	// Wild Gunman
 		{0x5112dc21,	SI_UNSET,		SI_ZAPPER,		SIFC_NONE		},	// Wild Gunman
 		{0xaf4010ea,	SI_GAMEPAD,		SI_POWERPADB,	SIFC_UNSET		},	// World Class Track Meet
+		{0x67b126b9,	SI_GAMEPAD,		SI_GAMEPAD,		SIFC_FAMINETSYS },	// Famicom Network System
 		{0x00000000,	SI_UNSET,		SI_UNSET,		SIFC_UNSET		}
 	};
 	int x = 0;
@@ -723,7 +724,8 @@ BMAPPINGLocal bmap[] = {
 	{"158B Prot Board",		258, UNL158B_Init},
 	{"F-15 MMC3 Based",		259, BMCF15_Init},
 	{"HP10xx/H20xx Boards",	260, BMCHPxx_Init},
-	{"810544-CA-1",			261, BMC810544CA1_Init},
+	{"810544-CA-1",		    261, BMC810544CA1_Init},
+
 	{ "KONAMI QTAi Board",	547, QTAi_Init },
 
 	{"",					0, NULL}
